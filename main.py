dd6281b8a6c5ee8cc6c27afa78b09be9a73932f6
Victor Mykhailov <victor.mykhailov@coxit.co>
2022-04-20 13:31:41 +0300

    Add new route - '/say'

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World'
+
+
+@app.route('/say')
+def hello_world():
+    word = request.args.get("word")
+    return word
 
 
 if __name__ == '__main__':
