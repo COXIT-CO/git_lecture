b3565a136e4031c26135b9bed1af2fb131f382c8
Victor Mykhailov <victor.mykhailov@coxit.co>
2022-04-20 13:27:53 +0300

    Add verification for request method

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello World'
+    if request.method == 'GET':
+        return 'Hello World'
 
 
 if __name__ == '__main__':
